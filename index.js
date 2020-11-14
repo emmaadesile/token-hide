1dcddb4f7ea2c604c96ff7716858bb0b486e08b2
Emmanuel Adesile <emma2adesile@gmail.com>
2020-11-14 16:56:17 +0100

    simple express app

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const https = require('http');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+
+https.createServer(app);
+
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+  const oauthToken = process.env.OAUTH_TOKEN;
+
+  res.send({
+    status: 'sucess',
+    token: oauthToken
+  })
+});
+
+app.listen(PORT, () => { 
+  console.log("server is running on", `http://localhost:${PORT}`); 
+});
